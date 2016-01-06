76be1f841a9c17b1cc719a8acc31b4df200c8b89
linlin <wllrenxue@126.com>
2016-01-06 12:33:35 +0800

    the 15th day, I didn't get the stack right, but others are all right

---
+++ day15/test.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class test{
+
+	Solution sl;
+
+	public test(){
+		int[] num1 = {-10,0, 4,7,0,17, 22,45, 0, 0 , 0, 0, 0};
+		int[] num2 = {0,3,7,12,38};
+		int[] num3 = {1};
+			
+		sl = new Solution();
+    sl.push(5);
+    //sl.swapPairs(sl.getListHead());
+    //sl.getIntersectionNode(sl.getListHead(), sl.getListHead());
+	}
+	public static void main(String[] args){
+		test t = new test();
+	}
+}
+
